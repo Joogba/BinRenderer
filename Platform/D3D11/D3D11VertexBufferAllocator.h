d27bbe2cbb941a3161c73e1c175cd7d0184b2cf2
Joogba <tjdwn0802@naver.com>
2025-07-11 00:01:40 +0900

    Transientbuffer Constant 와 Vertex / instance / index 용으로 분리

---
+++ Platform/D3D11/D3D11VertexBufferAllocator.h
@@ -0,0 +1,87 @@
+// 버텍스 , 인스턴스, 인덱스등 사용가능
+#pragma once
+#include "Core/ITransientBufferAllocator.h"
+#include <d3d11.h>
+#include <wrl/client.h>
+#include <cassert>
+
+namespace BinRenderer {
+
+    class D3D11VertexBufferAllocator : public ITransientBufferAllocator {
+    public:
+        D3D11VertexBufferAllocator(
+            ID3D11Device* device,
+            ID3D11DeviceContext* context,
+            uint32_t      initialBytes = 1 << 20 // 1MB default
+        )
+            : m_device(device), m_context(context), m_size(initialBytes)
+        {
+            createBuffer(m_size);
+        }
+
+        void beginFrame() override {
+            mapBuffer();
+            m_offset = 0;
+        }
+
+        void endFrame() override {
+            unmapBuffer();
+            m_offset = 0;
+        }
+
+        uint32_t alloc(uint32_t sizeBytes, void*& dataPtr, uint32_t align = 16) override {
+            uint32_t aligned = (m_offset + (align - 1)) & ~(align - 1);
+            if (aligned + sizeBytes > m_size) {
+                // 리사이즈 필요
+                resize(aligned + sizeBytes);
+                mapBuffer();
+            }
+            dataPtr = m_data + aligned;
+            m_offset = aligned + sizeBytes;
+            return aligned;
+        }
+
+        void* buffer() const override { return m_buffer.Get(); }
+
+    private:
+        void createBuffer(uint32_t bytes) {
+            D3D11_BUFFER_DESC desc{};
+            desc.ByteWidth = bytes;
+            desc.Usage = D3D11_USAGE_DYNAMIC;
+            desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+            desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+            desc.MiscFlags = 0;
+            HRESULT hr = m_device->CreateBuffer(&desc, nullptr, &m_buffer);
+            assert(SUCCEEDED(hr));
+            m_size = bytes;
+        }
+
+        void mapBuffer() {
+            D3D11_MAPPED_SUBRESOURCE mapped;
+            HRESULT hr = m_context->Map(m_buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &mapped);
+            assert(SUCCEEDED(hr));
+            m_data = reinterpret_cast<uint8_t*>(mapped.pData);
+        }
+
+        void unmapBuffer() {
+            m_context->Unmap(m_buffer.Get(), 0);
+            m_data = nullptr;
+        }
+
+        void resize(uint32_t requiredSize) {
+            unmapBuffer();
+            uint32_t newSize = m_size * 2;
+            while (newSize < requiredSize) newSize *= 2;
+            createBuffer(newSize);
+            mapBuffer();
+        }
+
+        Microsoft::WRL::ComPtr<ID3D11Buffer> m_buffer;
+        ID3D11Device* m_device;
+        ID3D11DeviceContext* m_context;
+        uint8_t* m_data = nullptr;
+        uint32_t m_size;
+        uint32_t m_offset = 0;
+    };
+
+}
