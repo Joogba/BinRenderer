17c96f68887a8ed70a8c962f29b897e0239edb77
Joogba <tjdwn0802@naver.com>
2025-09-16 17:33:21 +0900

    .

--- View.h
+++ View.h
@@ -17,24 +17,4 @@ namespace BinRenderer {
 		float maxDepth = 1.0f;
 	};
 
-	struct View
-	{
-		// 렌더 타겟 + 깊이뷰
-		std::vector<RenderTargetViewHandle> rtvs;   // 멀티타겟 지원
-		DepthStencilViewHandle dsv;
-
-		// 클리어 설정
-		uint32_t clearFlags = 0;          // enum class ClearFlags 비트마스크
-		uint32_t clearColor = 0xff000000; // AARRGGBB (or float[4])
-		float    clearDepth = 1.0f;
-		uint8_t  clearStencil = 0;
-
-		// 뷰포트
-		Viewport viewport;
-
-		std::string name;
-		// glm::mat4 viewMatrix;
-	   // glm::mat4 projMatrix;
-	};
-
 } // namespace BinRenderer
